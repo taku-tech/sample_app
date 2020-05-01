1adc1cb4b7ac54e786d74445ea949aa9af868a27
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-02 00:28:57 +0900

    [Add]画像の投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd023bad8741bd82a3d260ee041ffda38c84d3d1763b8a8825d3c999916221679a999c8a448afaa46ccf98fc1ad8feb589460781ade0994ac4cc1ac4fb30ef497'
